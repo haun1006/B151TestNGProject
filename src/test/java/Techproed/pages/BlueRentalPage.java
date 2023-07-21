396135a329db3518d92c0a8bb9692f7e8c654e9d
Hasan Ünver <hasanunver1006@gmail.com>
2023-07-21 20:28:42 +0300

    Initial commit

--- src/test/java/Techproed/pages/BlueRentalPage.java
+++ src/test/java/Techproed/pages/BlueRentalPage.java
@@ -1,18 +1,30 @@
 package Techproed.pages;
 
-import Techproed.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import Techproed.utilities.Driver;
 
 public class BlueRentalPage {
-    public BlueRentalPage(){      // Constructor olusturmus olduk.
-        PageFactory.initElements(Driver.getDriver(), this);
+    public BlueRentalPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
     }
     @FindBy(xpath = "(//*[@role='button'])[1]")
     public WebElement login;
     @FindBy(xpath = "//*[@name='email']")
     public WebElement email;
-    @FindBy(xpath = "//*[@type='button']")
+
+    @FindBy(xpath = "//*[@id='dropdown-basic-button']")
     public WebElement loginVerify;
+
+    @FindBy(xpath = "//*[@role='alert']")
+    public WebElement mesajVerify;
+
+    @FindBy(xpath = "//*[.='Logout']")
+    public WebElement logout;
+
+    @FindBy(xpath = "//*[.='OK']")
+    public WebElement ok;
+
+
 }
